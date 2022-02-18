da9c04ea2e9a9b092f57b0799dd4bb6489a807d0
NESS <vaness.535200052@stu.untar.ac.id>
2022-02-18 23:59:57 +0700

    push tugas 1

---
+++ flutter_application_2/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            Image(image: AssetImage('images/logo.png')),
+            Text('Hello World!'),
+          ],
+        )),
+      ),
+    );
+  }
+}
